9572c81f7a8b5dafbec9331bbe96df43f7512d83
hemanthnvd <hemanthnvd@gmail.com>
2024-06-15 23:34:52 -0400

    feat: add networking resources

---
+++ main.tf
@@ -0,0 +1,10 @@
+resource "aws_vpc" "main" {
+  cidr_block = var.vpc_cidr
+  tags = {
+    Name = var.vpc_name
+  }
+}
+
+resource "aws_internet_gateway" "gateway" {
+  vpc_id = aws_vpc.main.id
+}
---
+++ variables.tf
@@ -0,0 +1,42 @@
+variable "aws_region" {
+  type = string
+}
+variable "vpc_cidr" {
+  type = string
+}
+variable "vpc_name" {
+  type = string
+}
+variable "us-east-1a_availability_zone" {
+  type = string
+}
+variable "us-east-1c_availability_zone" {
+  type = string
+}
+variable "us-east-1e_availability_zone" {
+  type = string
+}
+variable "public_subnet_cidr" {
+  type = string
+}
+variable "private_subnet_cidr" {
+  type = string
+}
+variable "public_subnet_1_name" {
+  type = string
+}
+variable "public_subnet_2_name" {
+  type = string
+}
+variable "public_subnet_3_name" {
+  type = string
+}
+variable "private_subnet_1_name" {
+  type = string
+}
+variable "private_subnet_2_name" {
+  type = string
+}
+variable "private_subnet_3_name" {
+  type = string
+}
