19b02ee17aa79726a8e0888d78e9ffdd5c42a867
Eashan Roy <roy.ea@northeastern.edu>
2024-06-17 05:38:47 -0400

    chore: iam role for node group

---
+++ iam_role_node_group.tf
@@ -0,0 +1,21 @@
+# IAM Role for Node Group
+resource "aws_iam_role" "eks_worker_node_role" {
+  name = "eksWorkerNodeRole"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect = "Allow"
+      Principal = {
+        Service = "ec2.amazonaws.com"
+      }
+      Action = "sts:AssumeRole"
+    }]
+  })
+
+  managed_policy_arns = [
+    "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy",
+    "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly",
+    "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
+  ]
+}
