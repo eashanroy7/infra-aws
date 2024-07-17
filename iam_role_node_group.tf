1a5848ff1afa5fee687863aa5932edeff90c365b
Eashan Roy <roy.ea@northeastern.edu>
2024-07-17 03:44:23 -0400

    chore: trust relationship for OIDC, policies for autoscaler

--- iam_role_node_group.tf
+++ iam_role_node_group.tf
@@ -4,18 +4,76 @@ resource "aws_iam_role" "eks_worker_node_role" {
 
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
-    Statement = [{
-      Effect = "Allow"
-      Principal = {
-        Service = "ec2.amazonaws.com"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+        Action = "sts:AssumeRole"
+      },
+      # Enables integration with OIDC which allows Kubernetes service accounts to assume this role.
+      {
+        Effect = "Allow"
+        Principal = {
+          Federated = "${module.eks.oidc_provider_arn}" # Utilizes the terraform output variable 'oidc_provider_arn' from the EKS module
+        }
+        Action = "sts:AssumeRoleWithWebIdentity"
+        Condition = {
+          StringEquals = {
+            "${module.eks.oidc_provider}:sub": "system:serviceaccount:kube-system:cluster-autoscaler",
+            "${module.eks.oidc_provider}:aud": "sts.amazonaws.com"
+          }
+        }
       }
-      Action = "sts:AssumeRole"
-    }]
+    ]
   })
 
+  # Predefined policies provided by AWS
   managed_policy_arns = [
     "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy",
     "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly",
     "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
   ]
+}
+
+# Custom IAM policy for Cluster Autoscaler
+resource "aws_iam_policy" "cluster_autoscaler_policy" {
+  name        = "ClusterAutoscalerPolicy"
+  description = "Allows Cluster Autoscaler to manage Auto Scaling groups"
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    "Statement": [
+    {
+      "Effect": "Allow",
+      "Action": [
+        "autoscaling:DescribeAutoScalingGroups",
+        "autoscaling:DescribeAutoScalingInstances",
+        "autoscaling:DescribeLaunchConfigurations",
+        "autoscaling:DescribeScalingActivities",
+        "ec2:DescribeImages",
+        "ec2:DescribeInstanceTypes",
+        "ec2:DescribeLaunchTemplateVersions",
+        "ec2:GetInstanceTypesFromInstanceRequirements",
+        "eks:DescribeNodegroup"
+      ],
+      "Resource": ["*"]
+    },
+    {
+      "Effect": "Allow",
+      "Action": [
+        "autoscaling:SetDesiredCapacity",
+        "autoscaling:TerminateInstanceInAutoScalingGroup"
+      ],
+      "Resource": ["*"]
+    }
+  ]
+  })
+}
+
+# Attach custom policy to the Node Group IAM Role
+resource "aws_iam_role_policy_attachment" "cluster_autoscaler_policy_attachment" {
+  role       = aws_iam_role.eks_worker_node_role.name
+  policy_arn = aws_iam_policy.cluster_autoscaler_policy.arn
 }
