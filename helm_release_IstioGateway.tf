7304ec7b672cc52cc191979c194b9d5690ca2fb4
Eashan Roy <roy.ea@northeastern.edu>
2024-07-31 03:48:23 -0400

    feat: bootstrapping cluster with Istio Gateway Helm chart

---
+++ helm_release_IstioGateway.tf
@@ -0,0 +1,19 @@
+# helm repo add istio https://istio-release.storage.googleapis.com/charts
+# helm repo update
+# helm search repo istio/gateway
+# helm show values istio/gateway --version 1.17.1 > helm-defaults/gateway-default.yaml
+# helm install gateway -n istio-ingress --create-namespace istio/gateway
+resource "helm_release" "gateway" {
+  name = "gateway"
+
+  repository       = "https://istio-release.storage.googleapis.com/charts"
+  chart            = "gateway"
+  namespace        = "istio-ingress"
+  create_namespace = true
+  version          = "1.17.1"
+
+  depends_on = [
+    helm_release.istio_base,
+    helm_release.istiod
+  ]
+}
