fc5bad8dffb035f35172e8e9285d4c1940435f8b
Eashan Roy <roy.ea@northeastern.edu>
2024-08-02 03:31:10 -0400

    chore: updated null resource name to avoid conflict

--- helm_release_cluster_autoscaler.tf
+++ helm_release_cluster_autoscaler.tf
@@ -1,4 +1,4 @@
-resource "null_resource" "clone_repo" {
+resource "null_resource" "clone_repo_autoscaler" {
   provisioner "local-exec" {
     command = "git clone https://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@github.com/csye7125-su24-team17/helm-eks-autoscaler.git ./modules/charts/helm-eks-autoscaler"
   }
@@ -21,7 +21,7 @@ resource "kubernetes_secret" "image_pull_secret" {
 resource "helm_release" "cluster_autoscaler" {
   name       = "cluster-autoscaler"
   repository = "https://github.com/csye7125-su24-team17/helm-eks-autoscaler.git"
-  chart      = var.chart_path
+  chart      = var.chart_path_autoscaler
   version    = "0.1.0"
   # namespace = kubernetes_namespace.cluster-autoscaler-namespace.metadata[0].name
   namespace = "kube-system"
